46012f4ee568d8da360bd9a78a7436f7462df386
Ido Greenberg <idogreenberg90@gmail.com>
2019-04-16 14:34:45 +0300

    data description

--- data.py
+++ data.py
@@ -1,65 +1,143 @@
 import numpy as np
 import scipy as sp
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
 import re
 from time import time
 from pprint import pprint
-import io
+import os
 from warnings import warn
 import utils
+from InteractivePlotter import InteractiveFigure
 
-def load_data(fname=r'Data/train.csv', nrows=None):
-    df = pd.read_csv(fname, nrows=nrows)
+def load_data(fname=r'Data/train.csv', nrows=None, skiprows=None):
+    df = pd.read_csv(fname, nrows=nrows, skiprows=skiprows)
     df.rename(columns={'acoustic_data': 'signal',
                        'time_to_failure': 'quaketime'},
               inplace=True)
     return df
 
-def plot_data(df, cols=None, ax=None):
+def load_multi_files(dir=r'Data/test/', n_files=10, i0=0):
+    fnames = [dir + '/' + nm for nm in os.listdir(dir)][i0:i0+n_files]
+    dfs = []
+    for i,nm in enumerate(fnames):
+        df = load_data(nm)
+        df['segment'] = i0+i
+        dfs.append(df)
+    return pd.concat(dfs)
+
+def plot_data(df, cols=None, split_col=None, title='', ax=None):
     # initialization
+    mpl.rcParams['agg.path.chunksize'] = int(1e7)
     if ax is None:
         fig, ax = plt.subplots()
+    if isinstance(ax,str) and ax == 'interactive':
+        ax = InteractiveFigure().get_axes()
     if cols is None:
         cols = df.columns[:2]
     n = df.shape[0]
     double_y = (not isinstance(cols,str)) and len(cols)>1
-    title = 'Points: '
-    title += f'{n:d}' if n<1e3 \
-        else f'{n/1e3:.1f}K' if n<1e6 else f'{n/1e6:.1f}M'
-
+    n_samples = utils.counter_to_str(n)
+    tit = title+'\n' if title else title
+    tit += f'({n_samples:s} samples)'
+    shape = '-' if n<=150e3 else ','
 
     # first axis
     col = cols[0] if double_y else cols
-    ax.plot(np.arange(len(df[col])), df[col], 'b-')
-    ax.set_title('Points: ', fontsize=14)
+    ax.plot(np.arange(len(df[col])), df[col], 'b'+shape)
+    ax.grid()
+    ax.set_title(tit, fontsize=14)
     ax.set_xlabel('Sample', fontsize=12)
     ax.set_ylabel(col, color='b')
     ax.tick_params('y', colors='b')
+
+    # vertical splitting lines
+    if split_col:
+        M = np.max(np.abs(df[col]))
+        ids = np.where(np.diff(df[split_col])!=0)
+        for i in ids:
+            ax.plot((i+1,i+1), (-M,M), 'k-')
 
     # second axis
     if double_y:
         ax2 = ax.twinx()
         col = cols[1]
-        ax2.plot(np.arange(len(df[col])), df[col], 'r-')
+        ax2.plot(np.arange(len(df[col])), df[col], 'r'+shape)
         ax2.set_ylabel(col, color='r')
         ax2.tick_params('y', colors='r')
 
     utils.draw()
 
+def plot_distributions(signals, ax=None, quantiles=1000, title='', logscale=False):
+    if not isinstance(signals, dict): signals = {'signal':signals}
+    if ax is None:
+        fig, ax = plt.subplots()
+    if isinstance(ax,str) and ax == 'interactive':
+        ax = InteractiveFigure().get_axes()
+    if title: title+='\n'
+    if len(signals) == 1:
+        title += f'({len(list(signals.values())[0]):d} samples)'
+    else:
+        lengths = ', '.join([f'{len(signals[sig]):d}' for sig in signals])
+        title += f'({lengths:s} samples respectively)'
+
+    for i,sig in enumerate(signals):
+        signal = np.log10(1+np.abs(signals[sig])) * np.sign(signals[sig]) \
+            if logscale else signals[sig]
+        dist = utils.dist(signal, 100*np.arange(quantiles+1)/quantiles)
+        ax.plot((0,100), 2*[dist[1]], color=utils.DEF_COLORS[i], linestyle=':')
+        ax.plot(100*np.arange(quantiles+1)/quantiles, dist[2:],
+                color=utils.DEF_COLORS[i], linestyle='-', label=sig)
+
+    ax.set_xlim((0,100))
+    ax.set_xlabel('Quantile [%]', fontsize=12)
+    ax.set_ylabel('Log-signal [base 10]' if logscale else 'Signal', fontsize=12)
+    ax.set_title(title, fontsize=14)
+    if len(signals) > 1:
+        ax.legend()
+    utils.draw()
+
 
 if __name__ == '__main__':
-    df = load_data(nrows=10e6)
-    plot_data(df)
+    # load data
+    N = 10e6 # 10e6, 1e9
+    t0 = time()
+    df = load_data(nrows=N)
+    print(f'Train data loaded ({time()-t0:.0f} [s])')
+    dt = load_multi_files(i0=int(np.random.uniform(0,1000)))
+    print(f'Test data loaded ({time()-t0:.0f} [s])')
+
+    # plot signals
+    if False:
+        plot_data(df.head(int(10e6)), ax='interactive', title='Beginning of train')
+        plot_data(df.iloc[[1000*i for i in range(int(min(1e5,len(df)/1e3)))],:],
+                  ax='interactive', title='Beginning of train with skips (1:1000)')
+        i0 = int(np.random.uniform(0,10e6))
+        plot_data(df[i0:i0+int(150e3)], ax='interactive',
+                  title='Random segment in train')
+        plot_data(dt, cols='signal', split_col='segment', ax='interactive',
+                  title='Random adjacent segments in test')
+    print(f'Signals plotted ({time()-t0:.0f} [s])')
+
+    # plot distributions
+    # plot_distributions({'train':df.signal,'test':dt.signal}, ax='interactive',
+    #                    title='Sample of train vs. test signals')
+    plot_distributions({'train':df.signal,'test':dt.signal}, ax='interactive',
+                       title='Sample of train vs. test signals (log)', logscale=True)
+    # utils.qqplot(df.signal, dt.signal, ('train','test'), ax='interactive',
+    #        title='QQ-plot: train vs. test')
+    # utils.qqplot(df.signal, dt.signal, ('train','test'), ax='interactive',
+    #        title='QQ-plot: train vs. test (log)', logscale=True)
+    print(f'Distributions plotted ({time()-t0:.0f} [s])')
+
+    # plot FFTs
+    # TODO
+
     plt.show()
 
 
 # TODO:
-
-# plot train data in interesting resolutions
-# plot test data as a long sequence and mark the segment transitions
-# plot train & test signal distributions
-#    both separately (using twinx?) and as QQ-plot
 
 # compare fft plot of train segment with various methods:
 #    as raw; various interpolations; separated and then averaged
@@ -67,6 +145,3 @@ if __name__ == '__main__':
 # plot distribution of the strongest frequency
 #    over 4K blocks / 150K segments?
 # compare train & test ffts
-
-# try to detect test segments which look continuous
-#    (according to Zahar's heuristic)?
---
+++ utils.py
@@ -0,0 +1,263 @@
+import pandas as pd
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import networkx as nx
+from scipy.linalg import eigh
+import re
+import time
+from pprint import pprint
+from warnings import warn
+from datetime import datetime
+import itertools
+import bidi.algorithm as bidi
+import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
+from InteractivePlotter import InteractiveFigure
+
+
+##################   STATS UTILS   ##################
+
+def dist(x, quantiles=(0,10,50,90,100), do_round=False):
+    '''
+    Get the distribution of a vector as quantiles + very basic stats.
+    :param x: numeric numpy array.
+    :param quantiles: quantiles in [0,100] to find.
+    :param do_round: whether to round numbers or not.
+    :return: a list [len(x), mean(x), quant1(x), quant2(x), ..., quantn(x)].
+    '''
+    if not len(x):
+        return [None for _ in range(2+len(quantiles))]
+    s = [len(x), np.mean(x)] + list(np.percentile(x,quantiles))
+    return [int(z+np.sign(z)*0.5) for z in s] if do_round else s
+
+def table(l, sort=0):
+    '''
+    :param l: iterable object.
+    :param sort: 0 for no sort; +-1 for increasing/decreasing frequency respectively.
+    :return: tuple of 2D-tuples (value, number of occurences in l).
+    '''
+    try:
+        # if it can be sorted, it should (otherwise groupby may work unexpectedly).
+        tab = tuple((g[0], len(list(g[1])))
+                    for g in itertools.groupby(sorted(l)))
+    except:
+        # if it can't be sorted (e.g. datetime), try groupby anyway.
+        tab = tuple((g[0], len(list(g[1])))
+                    for g in itertools.groupby(l))
+    if sort:
+        tab = sorted(tab, key = lambda x: np.sign(sort)*x[1])
+    return tab
+
+def info(x, **kwargs):
+    if isinstance(x, pd.DataFrame): info_data_frame(x, **kwargs)
+    elif isinstance(x, nx.Graph): info_graph(x, **kwargs)
+    else: print(f'Unsupported type: {type(x):s}')
+
+def info_data_frame(df, verbose=1):
+    assert(isinstance(df,pd.DataFrame)),\
+        f'Bad type (not a DataFrame): {type(df):s}'
+    print(f'\n\nData-Frame information:')
+    # data frame info
+    print(f'Dimensions: \t{df.shape[0]} X {df.shape[1]}')
+    print('Columns:', end='\n\t')
+    for i,col in enumerate(df.columns):
+        print(col, end =
+        '\n\t' if i+1==df.shape[1] or (i+1)%3==0 else '  |  ')
+    print('')
+    # columns stats
+    if verbose >= 1:
+        for col in df.columns:
+            print(f'{col:s}:\n\tTypes: \t', end='')
+            types = table([str(type(x))[8:-2] for x in df[col]], -1)
+            for t in types:
+                print(f'{t[0]:s} ({100*t[1]/len(df):.0f}%), ', end='')
+            print('')
+            print(f'\tUnique values: \t{len(np.unique(df[col])):d}')
+            print('\tFrequent values: \t', end='')
+            tab = table(df[col],-1)
+            for i,t in enumerate(tab[:4]):
+                try:
+                    if len(t[0])>30:
+                        tab[i] = (str(t[0][:30])+'...', tab[i][1])
+                except:
+                    pass
+            print(*[f'{t[0]}'+f' ({t[1]:d}),' for t in tab[:4]])
+            print('\tFrequent frequencies: \t', end='')
+            tab = table([t[1] for t in tab], -1)
+            print(*[f'{t[0]:d}'+f' ({t[1]:d}),' for t in tab[:4]])
+            qs = (0, 10, 50, 90, 100)
+            try:
+                d = dist(df[col])
+                print(f'\tAverage: \t{d[1]}')
+                print('\tQuantiles: \t', end='')
+                print(*[f'{q:.0f}%: {v}' for q, v in zip(qs, d[2:])],
+                      sep=',   ')
+            except:
+                try:
+                    d = dist([len(s) for s in df[col]])
+                    print(f'\tAverage length: \t{d[1]}')
+                    print('\tQuantiles: \t', end='')
+                    print(*[f'{q:.0f}%: {v:.0f}'
+                            for q, v in zip(qs, d[2:])],
+                          sep=',   ')
+                except:
+                    pass
+    # sample of contents
+    if verbose >= 2:
+        if (len(df)<20):
+            print(df)
+        else:
+            print('\nHead:')
+            print(df.head(5))
+            print('\nMid:')
+            print(df[int(len(df)/2)-2:int(len(df)/2)+2])
+            print('\nTail:')
+            print(df.tail(5))
+        print('')
+    # add optional plots if verbose >= 3?
+
+def info_graph(G, verbose=1):
+    print(f'\n\nGraph information:')
+    n = G.number_of_nodes()
+    if n==0:
+        print('Empty graph.')
+        return
+    m = G.number_of_edges()
+    self_loops = G.number_of_selfloops()
+    print(f'Nodes: {n:d}\t\tEdges: {m:d}\t\t' +
+          f'Density: {100*(m-self_loops)/sp.special.binom(n,2):.0f}%\t\t' +
+          f'Self-loops: {100*self_loops/n:.0f}%')
+    CC = nx.algorithms.components.number_connected_components(G)
+    C = max(nx.connected_component_subgraphs(G), key=len)
+    print(f'Connectivity components: {CC:d}\t\t' +
+          f'Largest component: {len(C):d} nodes')
+    print('Clustering coefficient:\t{0:.0f}%'.format(
+        100*nx.algorithms.cluster.average_clustering(G)))
+    print('Distribution of degrees:')
+    print(*[0,10,50,90,100], sep='%\t')
+    print(dist(list(G.degree().values()), do_round=True)[2:])
+    if verbose >= 2:
+        print('Largest component diameter:\t{0:d}'.format(
+            nx.algorithms.distance_measures.diameter(C) ))
+        # Spectrum
+        L = nx.laplacian_matrix(G)
+        x, v = eigh(L.todense())
+        print('Spectral-estimated number of clusters:\t{0:d}'.format(
+            np.argmax(np.diff(x)) + 1 ))
+        if n <= 100:
+            fig, ax = plt.subplots(1, 1)
+            barplot(ax, list(range(n)), x, ylab='Eigenvalue', colors='b')
+            draw()
+        else:
+            print('Distribution of Laplacian spectrum:')
+            print(*[0,10,50,90,100], sep='%\t')
+            print(dist(x)[2:])
+        if verbose >= 3:
+            if n <= 1000:
+                fig, ax = plt.subplots(1, 1)
+                plt.axes(ax)
+                Gtmp = nx.relabel_nodes(
+                    G, {w: bidi.get_display(w) for w in G.node}, True)
+                nx.draw(Gtmp, with_labels=True, edge_color='green')
+                if verbose >= 4:
+                    # spectral embedding of both Graph & largest component
+                    fig, ax = plt.subplots()
+                    nx.draw(Gtmp, pos={w: (v[i, 1], v[i, 2])
+                                    for i,w in enumerate(Gtmp.node.keys())},
+                            with_labels=True, edge_color='green')
+                    Ctmp = max(nx.connected_component_subgraphs(Gtmp), key=len)
+                    fig, ax = plt.subplots()
+                    nx.draw(Ctmp, pos={w: (v[i, 1], v[i, 2])
+                                    for i,w in enumerate(Ctmp.node.keys())},
+                            with_labels=True, edge_color='green')
+
+def count(txt, sep):
+    '''
+    Count tokens in text.
+    :param txt: string or iterable of strings.
+    :param sep: separator for text.
+    :return: count of tokens or list of counts.
+    '''
+    if isinstance(txt,str):
+        return len(list(filter(None,re.split(sep,txt))))
+    else:
+        return [len(list(filter(None,re.split(sep,s)))) for s in txt]
+
+##################   PLOT UTILS   ##################
+
+def draw():
+    plt.get_current_fig_manager().window.showMaximized()
+    plt.draw()
+    plt.pause(1e-10)
+    plt.tight_layout()
+
+def clean_figure(ax):
+    '''
+    Clean an empty figure (remove x,y axes and ticks).
+    '''
+    ax.set_xticks(())
+    ax.set_yticks(())
+    ax.set_xticklabels(())
+    ax.set_yticklabels(())
+
+DEF_COLORS = ('blue','red','green','purple','orange','grey','pink')
+
+def barplot(ax, x, y, bottom=None, plot_bottom=True,
+            title=None, xlab=None, ylab=None, xlim=None, ylim=None, label=None,
+            vertical_xlabs=False, colors=DEF_COLORS, bcolors=DEF_COLORS):
+    '''
+    pyplot's barplot wrapper.
+    '''
+    xticks = None
+    if any((isinstance(xx,str) for xx in x)):
+        xnames = x
+        x = tuple(range(len(x)))
+        xticks = x
+        xlim = (-0.5,len(xnames)-0.5)
+    if bottom is not None and plot_bottom:
+        ax.bar(x, bottom,
+               color=bcolors[:len(x)] if isinstance(bcolors,tuple) else bcolors)
+    ax.bar(x, y, bottom=bottom, label=label,
+           color=colors[:len(x)] if isinstance(colors,tuple) else colors)
+    if title: ax.set_title(title, fontsize=14)
+    if xlab: ax.set_xlabel(xlab, fontsize=12)
+    if ylab: ax.set_ylabel(ylab, fontsize=12)
+    if xlim: ax.set_xlim(xlim[0],xlim[1])
+    if ylim: ax.set_ylim(ylim[0],ylim[1])
+    if xticks:
+        ax.set_xticks(xticks)
+        ax.set_xticklabels(xnames, fontsize=10)
+        if vertical_xlabs:
+            for tick in ax.get_xticklabels():
+                tick.set_rotation(90)
+
+def qqplot(x1, x2, names, ax=None, quantiles=1000, title='', logscale=False):
+    if ax is None:
+        fig, ax = plt.subplots()
+    if isinstance(ax,str) and ax == 'interactive':
+        ax = InteractiveFigure().get_axes()
+    if logscale:
+        x1 = np.log10(1 + np.abs(x1)) * np.sign(x1)
+        x2 = np.log10(1 + np.abs(x2)) * np.sign(x2)
+
+    M = max(np.max(np.abs(x1)),np.max(np.abs(x2)))
+    dist1 = dist(x1, 100*np.arange(quantiles+1)/quantiles)
+    dist2 = dist(x2, 100*np.arange(quantiles+1)/quantiles)
+    ax.plot((-M,M), (-M,M), 'k--')
+    ax.plot(dist1[2:], dist2[2:], 'b.-')
+    ax.plot((dist1[1],), (dist2[1],), 'ro')
+    dist1 = dist(x1, 10*np.arange(11))
+    dist2 = dist(x2, 10*np.arange(11))
+    for i in range(11):
+        ax.annotate(f'{10*i:.0f}%', (dist1[2+i], dist2[2+i]),
+                    horizontalalignment='right')
+    ax.set_xlabel(('Log-' if logscale else '')+names[0]+
+                  f' ({counter_to_str(len(x1)):s} samples)', fontsize=12)
+    ax.set_ylabel(('Log-' if logscale else '')+names[1]+
+                  f' ({counter_to_str(len(x2)):s} samples)', fontsize=12)
+    ax.set_title(title, fontsize=14)
+    draw()
+
+def counter_to_str(n):
+    return f'{n:d}' if n<1e3 else f'{n/1e3:.2f}K' if n<1e6 else f'{n/1e6:.2f}M'
